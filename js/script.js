0d52789834a2ba3cc4ef980c9ac8a28be668696f
Stacy Weru <stacyweru@gmail.com>
2020-04-05 14:03:23 +0300

    Add function validate to scripts.js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function validate();{
+    var date = parseInt(document.getElementById('dateInput').value);
+    var month = parseInt(document.getElementById('monthInput').value);
+    var year = parseInt(document.getElementById('yearInput').value);
+    var century = parseInt(document.getElementById('centuryInput').value);
+}
