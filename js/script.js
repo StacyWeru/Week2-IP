d897b33d02b76b247ef303f4544ace98876e40d8
Stacy Weru <stacyweru@gmail.com>
2020-04-05 16:54:18 +0300

    Add day formula to js

--- js/script.js
+++ js/script.js
@@ -4,10 +4,23 @@ var day = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday
 
 
 function validate();{
-    var date = parseInt(document.getElementById("dateInput").value);
-    var month = parseInt(document.getElementById("monthInput").value);
-    var year = parseInt(document.getElementById("yearInput").value);
-    var century = parseInt(document.getElementById("centuryInput").value);
+    var DD = parseInt(document.getElementById("dateInput").value);
+    var MM = parseInt(document.getElementById("monthInput").value);
+    var YY = parseInt(document.getElementById("yearInput").value);
+    var CC = parseInt(document.getElementById("centuryInput").value);
     var gender = document.getElementById("genderInput");
 }
+function calculateDayValue(){
+    day = (((CC/4) -2*CC -1) + ((5* YY/4)) + ((26*(MM + 1)/10)) + DD)% 7;
+    console.log(d);
+    return(Math.floor(day));
+}
 
+    if(gender[0].checked== true){
+        var gender = "maleAkan";
+
+    }else if(gender[1].checked == true){
+        var gender ="femaleAkan";
+    }else{
+        return false;
+    }
