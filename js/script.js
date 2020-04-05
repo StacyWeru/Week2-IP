83b733ef17b62a8fc759f819e0534fa4cda9f011
Stacy Weru <stacyweru@gmail.com>
2020-04-05 16:35:59 +0300

    Add variables maleAkan, femaleAkan and day

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
+var maleAkan = [ "Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+var femaleAkan = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+var day = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+
 function validate();{
-    var date = parseInt(document.getElementById('dateInput').value);
-    var month = parseInt(document.getElementById('monthInput').value);
-    var year = parseInt(document.getElementById('yearInput').value);
-    var century = parseInt(document.getElementById('centuryInput').value);
+    var date = parseInt(document.getElementById("dateInput").value);
+    var month = parseInt(document.getElementById("monthInput").value);
+    var year = parseInt(document.getElementById("yearInput").value);
+    var century = parseInt(document.getElementById("centuryInput").value);
+    var gender = document.getElementById("genderInput");
 }
+
