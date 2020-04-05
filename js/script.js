d64e7dd6e7730f32baf0c8094840811ab6cc1f86
Stacy Weru <stacyweru@gmail.com>
2020-04-05 21:44:52 +0300

    Add variables and formula finding the day

--- js/script.js
+++ js/script.js
@@ -4,15 +4,30 @@ var daysOfTheWeek = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday"
 
 
 function validate();{
-    var date = parseInt(document.getElementById("dateInput"));
-    var month = parseInt(document.getElementById("monthInput"));
-    var year = parseInt(document.getElementById("yearInput"));
-    var century = parseInt(document.getElementById("centuryInput"));
+    var date = parseInt(document.getElementById("dateInput").value);
+    var month = parseInt(document.getElementById("monthInput").value);
+    var year = parseInt(document.getElementById("yearInput").value);
+    var century = parseInt(document.getElementById("centuryInput").value);
     var gender = document.getElementById("genderInput");
 
-    if(date.value == "" || month.value == "" || year == "" || century.value == "" || gender.value == "" )
+    if(date.value == "" || month.value == "" || year == "" || century.value == "");
     {
         alert("Please fill in the blanks appropriately");
+        return false;
+    } else {
+
     }
-    else
 }
+    function calculate(){
+        var CC = century;
+        var YY = year;
+        var MM = month;
+        var DD = date;
+        var day = ( ( (CC/4) -2*CC-1) + ( (5*YY/4) ) + ((26*(MM+1)/10) ) + DD)%7;
+        console.log(day);
+        return (Math.floor(day));
+      }
+    }
+    
+
+     
